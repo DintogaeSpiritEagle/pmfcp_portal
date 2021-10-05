85b7d325b923fb36814a4ac2678520277ea53e69
Yamis SpiritEagle <yamis.spiriteagle@gmail.com>
2021-10-05 14:39:15 +1000

    MP-28 progressive updates. Customized jetstream routes and disable library defaults.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,6 +15,7 @@ use Inertia\Inertia;
 |
 */
 
+// Default route render
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -24,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+// Dashboard route protection
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+// Include customized jetstream routes
+require_once __DIR__ . '/jetstream.php';
